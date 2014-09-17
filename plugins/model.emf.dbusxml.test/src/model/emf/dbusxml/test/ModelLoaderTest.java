a395946523bf06961631d0376480e110b0b8bcad
Klaus Birken <klaus.birken@gmail.com>
2014-09-17 13:17:30 +0200

    Added test for support of annotations.

--- plugins/model.emf.dbusxml.test/src/model/emf/dbusxml/test/ModelLoaderTest.java
+++ plugins/model.emf.dbusxml.test/src/model/emf/dbusxml/test/ModelLoaderTest.java
@@ -8,9 +8,14 @@ import static org.junit.Assert.fail;
 import java.io.IOException;
 import java.util.Collections;
 
+import model.emf.dbusxml.AnnotationType;
+import model.emf.dbusxml.ArgType;
 import model.emf.dbusxml.DocumentRoot;
 import model.emf.dbusxml.InterfaceType;
+import model.emf.dbusxml.MethodType;
 import model.emf.dbusxml.NodeType;
+import model.emf.dbusxml.PropertyType;
+import model.emf.dbusxml.SignalType;
 import model.emf.dbusxml.util.DbusxmlResourceFactoryImpl;
 
 import org.eclipse.emf.common.util.URI;
@@ -37,6 +42,48 @@ public class ModelLoaderTest {
 		assertEquals(4, i.getMethod().size());
 		assertEquals(2, i.getSignal().size());
 		// TODO: add more checks here (e.g., names of elements, details, ...)
+	}
+
+	@Test
+	public void loadModel30() {
+		InterfaceType i = loadModel("models/30-Annotations.xml");
+		assertNotNull(i);
+
+		// annotations on interface level
+		assertEquals(1, i.getAnnotation().size());
+		AnnotationType annoInterface = i.getAnnotation().get(0);
+		assertEquals("OnInterface", annoInterface.getValue());
+		
+		// general structure of interface
+		assertEquals(1, i.getProperty().size());
+		assertEquals(1, i.getMethod().size());
+		assertEquals(1, i.getSignal().size());
+		
+		// annotations on properties
+		PropertyType p = i.getProperty().get(0);
+		assertEquals(1, p.getAnnotation().size());
+		AnnotationType annoProperty = p.getAnnotation().get(0);
+		assertEquals("OnProperty", annoProperty.getValue());
+
+		// annotations on methods
+		MethodType m = i.getMethod().get(0);
+		assertEquals(1, m.getAnnotation().size());
+		AnnotationType annoMethod = m.getAnnotation().get(0);
+		assertEquals("OnMethod", annoMethod.getValue());
+
+		// annotations on method arguments
+		assertEquals(2, m.getArg().size());
+		ArgType arg = m.getArg().get(0);
+//		assertEquals(1, arg.getAnnotation().size());
+//		AnnotationType annoArg = m.getAnnotation().get(0);
+//		assertEquals("OnArg", annoMethod.getValue());
+		
+		// annotations on signals
+		SignalType s = i.getSignal().get(0);
+		assertEquals(1, s.getAnnotation().size());
+		AnnotationType annoSignal = s.getAnnotation().get(0);
+		assertEquals("OnSignal", annoSignal.getValue());
+
 	}
 
 	
