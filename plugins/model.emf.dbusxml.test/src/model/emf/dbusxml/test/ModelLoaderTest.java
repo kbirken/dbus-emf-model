a8baf34a78ca3aa653829282f4dc78507bc27c50
Klaus Birken <klaus.birken@gmail.com>
2014-09-17 13:03:22 +0200

    Additional test for loading models.

---
+++ plugins/model.emf.dbusxml.test/src/model/emf/dbusxml/test/ModelLoaderTest.java
@@ -0,0 +1,67 @@
+package model.emf.dbusxml.test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+
+import java.io.IOException;
+import java.util.Collections;
+
+import model.emf.dbusxml.DocumentRoot;
+import model.emf.dbusxml.InterfaceType;
+import model.emf.dbusxml.NodeType;
+import model.emf.dbusxml.util.DbusxmlResourceFactoryImpl;
+
+import org.eclipse.emf.common.util.URI;
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.ecore.resource.Resource;
+import org.junit.Before;
+import org.junit.Test;
+
+public class ModelLoaderTest {
+
+	private DbusxmlResourceFactoryImpl factory;
+	
+	@Before
+	public void setUp() throws Exception {
+		factory = new DbusxmlResourceFactoryImpl();
+	}
+
+	@Test
+	public void loadModel20() {
+		InterfaceType i = loadModel("models/20-AllElements.xml");
+		assertNotNull(i);
+		
+		assertEquals(4, i.getProperty().size());
+		assertEquals(4, i.getMethod().size());
+		assertEquals(2, i.getSignal().size());
+		// TODO: add more checks here (e.g., names of elements, details, ...)
+	}
+
+	
+	private InterfaceType loadModel(String filename) {
+		Resource resource = factory.createResource(URI.createFileURI(filename));
+		try {
+			resource.load(Collections.EMPTY_MAP);
+			assertNotNull(resource.getContents());
+			
+			EObject root = resource.getContents().get(0);
+			assertNotNull(root);
+			assertTrue(root instanceof DocumentRoot);
+			
+			DocumentRoot doc = (DocumentRoot)root;
+			NodeType node = doc.getNode();
+			assertNotNull(node);
+			
+			assertEquals("Expecting exactly one interface", 1, node.getInterface().size());
+			return node.getInterface().get(0);
+			
+		} catch (IOException e) {
+			e.printStackTrace();
+			fail();
+			return null;
+		}
+	}
+
+}
